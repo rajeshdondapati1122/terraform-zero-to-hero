ec94809307cc36f4213fe72307c1613622d92152
rajeshdondapati1122 <163703768+rajeshdondapati1122@users.noreply.github.com>
2024-08-24 09:07:52 +0530

    Update main.tf
    region&ami changed

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-west-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-04fdea8e25817cd69"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
